55973fb0733fcdafca118cec8e416f16b153fae7
Eddy Lei <43884159+ededddy@users.noreply.github.com>
2021-01-31 21:03:52 +0800

    Initial commit

---
+++ src/App.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import IconButton from "@material-ui/core/IconButton";
+
+export const Wrapper = styled.div`
+  margin: 55px;
+`;
+
+export const StyledButton = styled(IconButton)`
+  position: absolute;
+  z-index: 100;
+  right: 20px;
+  top: 20px;
+`;
---
+++ src/Cart/Cart.styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.aside`
+  font-family: Arial, Helvetica, sans-serif;
+  width: 500px;
+  padding: 50px 20px 20px 20px;
+
+  @media screen and (max-width: 600px) {
+    width: 100%;
+    padding: 10px;
+  }
+`;
